a3b27039c1849e71970bda6e949b7a1ad30773c9
Kyle1993 <344927747@qq.com>
2018-12-09 12:31:02 +0800

    update

--- train_predict.py
+++ train_predict.py
@@ -2,29 +2,41 @@ from xgboost.sklearn import XGBClassifier
 import xgboost as xgb
 import pickle
 import numpy as np
-from sklearn.metrics import roc_auc_score
-from matplotlib import pyplot
+import time
+import pandas as pd
 
 train_data = np.load('train_data.npy')
 train_label = np.load('train_label.npy')
 test_data = np.load('test_data.npy')
-with open('best_param.pkl','rb') as f:
-    param = pickle.load(f)
 
-# load param
-model = XGBClassifier()
-model.set_params(**param)
-# train
-model.fit(train_data,train_label)
+print('Training...')
+params = {
+    'booster': 'gbtree',
+    'objective': 'binary:logistic',  # 多分类的问题
+    'gamma': 0.1,                 # 用于控制是否后剪枝的参数,越大越保守，一般0.1、0.2这样子。
+    'max_depth': 6,               # 构建树的深度，越大越容易过拟合
+    'subsample': 0.8,             # 随机采样训练样本
+    'colsample_bytree': 0.8,      # 生成树时进行的列采样
+    'min_child_weight': 2,
+    'silent': 1,                  # 设置成1则没有运行信息输出，最好是设置为0.
+    'eta': 0.1,                   # 如同学习率
+    'alpha':1e-5,
+    'lambda':1e-5,
+    'seed': 1000,
+    'scale_pos_weight':1,
+    'nthread': 4,                  # cpu 线程数
+}
 
-# show feature importance
-print(model.feature_importances_)
-print(len(model.feature_importances_))
-print(sum(model.feature_importances_))
-xgb.plot_importance(model)
-pyplot.show()
+dtrain = xgb.DMatrix(train_data, label=train_label,)
+num_round = 200
+bst = xgb.train(params, dtrain, num_round)
 
-# predict
-pre = model.predict_proba(test_data)[:,1]
-# print('AUC:',roc_auc_score(train_label,pre))
-print(pre)
+print('Predicting...')
+dtest = xgb.DMatrix(test_data,)
+pred = bst.predict(dtest)
+
+print('Generating CSV file...')
+submit = pd.DataFrame({'UID':test_id,'Tag':pred})
+submit = submit.set_index('UID',drop=True)
+time_str = time.strftime("%Y-%m-%d-%H-%M", time.localtime())
+submit.to_csv('submit_{}.csv'.format(time_str))
